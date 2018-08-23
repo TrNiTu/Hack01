af1e635f5522f2719e03af1bf5b5823427d6c28d
Trey Turner <treyniturner@gmail.com>
2018-08-23 14:21:35 -0500

    Hello World

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Trey Turner
+ * Date: 2018/8/21
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Trey Turner\n");
+  printf("I'm majoring in Computer Science\n");
+
+  return 0;
+}
